2b86d7cf121455a3b76e3a9357597d7eb1050cef
raviteja <teja4360@gmail.com>
2023-05-18 15:26:43 +0530

    Random txt

--- servers.tf
+++ servers.tf
@@ -1,21 +1,21 @@
 resource "aws_instance" "app_instance" {
   for_each = var.app_servers
   ami                    = data.aws_ami.ec2_ami.image_id
-  instance_type          = each.app_servers["type"]
+  instance_type          = each.value["type"]
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
-    Name = each.app_servers["name"]
+    Name = each.value["name"]
   }
 }
 
 resource "aws_instance" "db_instance" {
   for_each = var.db_instance_servers
   ami                    = data.aws_ami.ec2_ami.image_id
-  instance_type          = each.db_instance_servers["type"]
+  instance_type          = each.value["type"]
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
-    Name = each.db_instance_servers["type"]
+    Name = each.value["name"]
   }
 }
