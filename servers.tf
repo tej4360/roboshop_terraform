5a14100093daa8d3509f5d63d8c145857c63949f
raviteja <teja4360@gmail.com>
2023-05-18 18:22:05 +0530

    Random txt

--- servers.tf
+++ servers.tf
@@ -20,3 +20,21 @@ resource "aws_instance" "db_instance" {
     Name = each.value["name"]
   }
 }
+
+resource "aws_route53_record" "app_route53" {
+  for_each = var.app_servers
+  zone_id = "Z05398713LIRV3MCPOPMB"
+  name    = "${each.key}rtdevopspract.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.aws_instance[each.value["name"]].private_ip]
+}
+
+resource "aws_route53_record" "db_route53" {
+  for_each = var.db_servers
+  zone_id = "Z05398713LIRV3MCPOPMB"
+  name    = "${each.key}rtdevopspract.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.aws_instance[each.value["name"]].private_ip]
+}
