53c10713e04aa9439b78fda8cce57f4b240e2eeb
raviteja <teja4360@gmail.com>
2023-05-18 12:07:43 +0530

    Random txt

--- servers.tf
+++ servers.tf
@@ -1,4 +1,13 @@
 module "app_servers" {
+  depends_on = [module.db_servers]
+  for_each = var.app_servers
+  source = "./module"
+  component_name = each.value["name"]
+  instance_type = each.value["type"]
+}
+
+
+module "db_servers" {
   for_each = var.app_servers
   source = "./module"
   component_name = each.value["name"]
--- vars.tf
+++ vars.tf
@@ -10,7 +10,45 @@ variable "app_servers" {
       name = "catalogue"
       type = "t3.micro"
     }
+    cart = {
+      name = "cart"
+      type = "t3.micro"
+    }
+    payment = {
+      name = "payment"
+      type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      type = "t3.micro"
+    }
+    shipping = {
+      name = "shipping"
+      type = "t3.micro"
+    }
   }
 }
 
-#variable "db_servers" {}
+variable "db_servers" {
+  defualt = {
+    mangodb = {
+      name = "mangodb"
+      type = "t3.micro"
+    }
+
+    reddis = {
+      name = "reddis"
+      type = "t3.micro"
+    }
+
+    rabbitmq = {
+      name = "rabbitmq"
+      type = "t3.micro"
+    }
+
+    mysql = {
+      name = "mysql"
+      type = "t3.micro"
+    }
+  }
+}
