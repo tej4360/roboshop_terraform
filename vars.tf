0d5e12ee55c40b2c0cdef3ab7367394b7d71a486
raviteja <teja4360@gmail.com>
2023-05-18 12:03:47 +0530

    Random txt

--- vars.tf
+++ vars.tf
@@ -1,13 +1,15 @@
 #variable "env" {}
 
 variable "app_servers" {
-  frontend = {
-    name = "frontend"
-    type = "t3.micro"
-  }
-  catalogue = {
-    name = "catalogue"
-    type = "t3.micro"
+  default = {
+    frontend = {
+      name = "frontend"
+      type = "t3.micro"
+    }
+    catalogue = {
+      name = "catalogue"
+      type = "t3.micro"
+    }
   }
 }
 
