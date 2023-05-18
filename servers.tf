e604e705d03860acd5ddd9917c9602c866ed2719
raviteja <teja4360@gmail.com>
2023-05-18 17:22:43 +0530

    Random txt

--- servers.tf
+++ servers.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "app_instance" {
+  depends_on = aws_instance.db_instance
   for_each = var.app_servers
   ami                    = data.aws_ami.ec2_ami.image_id
   instance_type          = each.value["type"]
@@ -10,7 +11,7 @@ resource "aws_instance" "app_instance" {
 }
 
 resource "aws_instance" "db_instance" {
-  for_each = var.db_instance_servers
+  for_each = var.db_servers
   ami                    = data.aws_ami.ec2_ami.image_id
   instance_type          = each.value["type"]
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
