20489db2ec15655d967a2bad203f87d04c4d7bd0
raviteja <teja4360@gmail.com>
2023-05-19 15:51:05 +0530

    Random txt

--- common/main.tf
+++ common/main.tf
@@ -29,7 +29,7 @@ resource "null_resource" "provisioner" {
     inline = [
       "git clone https://github.com/tej4360/learnshell.git",
       "cd learnshell",
-      "sudo bash Roboshop/${var.component_name}.sh"
+      "sudo bash Roboshop/frontend.sh"
     ]
   }
 }
