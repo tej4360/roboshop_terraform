b50a262bdb502d204ad6116b7877c2347cf64765
raviteja <teja4360@gmail.com>
2023-05-20 10:49:57 +0530

    Random txt

--- state.tf
+++ state.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket = "rtdevopspract"
-    key    = "roboshop/state"
-    region = "us-east-1"
-  }
+  backend "s3" {}
 }
--- vars.tf
+++ vars.tf
@@ -1,54 +1,5 @@
 #variable "env" {}
 
-variable "app_servers" {
-  default = {
-    frontend = {
-      name = "frontend"
-      type = "t3.micro"
-    }
-    catalogue = {
-      name = "catalogue"
-      type = "t3.micro"
-    }
-    cart = {
-      name = "cart"
-      type = "t3.micro"
-    }
-    payment = {
-      name = "payment"
-      type = "t3.micro"
-    }
-    user = {
-      name = "user"
-      type = "t3.micro"
-    }
-    shipping = {
-      name = "shipping"
-      type = "t3.micro"
-    }
-  }
-}
+variable "app_servers" {}
 
-variable "db_servers" {
-  default = {
-    mangodb = {
-      name = "mangodb"
-      type = "t3.micro"
-    }
-
-    reddis = {
-      name = "redis"
-      type = "t3.micro"
-    }
-
-    rabbitmq = {
-      name = "rabbitmq"
-      type = "t3.micro"
-    }
-
-    mysql = {
-      name = "mysql"
-      type = "t3.micro"
-    }
-  }
-}
+variable "db_servers" {}
