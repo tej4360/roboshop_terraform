c49a36184ff32bc45a037c04473f463d40f63361
raviteja <teja4360@gmail.com>
2023-07-11 22:32:43 +0530

    Random txt

--- common/locals.tf
+++ common/locals.tf
@@ -11,4 +11,12 @@ locals {
     "sudo labauto ansible",
     "ansible-pull -i localhost, -U https://github.com/tej4360/roboshop_ansible.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
+
+  app_tags = {
+    Name = "${var.env}-${var.component_name}"
+  }
+
+  db_tags = {
+    Name = "${var.env}-${var.component_name}"
+  }
 }
