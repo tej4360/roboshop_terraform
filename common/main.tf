568fef6d3ae8e04736f4d163ff24c3c2797836f0
raviteja <teja4360@gmail.com>
2023-07-11 23:00:23 +0530

    Random txt

--- common/main.tf
+++ common/main.tf
@@ -3,10 +3,10 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
   iam_instance_profile = aws_iam_instance_profile.iam_instance_profile.name
-#  tags = var.instance_type == "app" ? local.app_tags : local.db_tags
-  tags = {
-    Name = var.component_name
-  }
+  tags = var.instance_type == "app" ? local.app_tags : local.db_tags
+#  tags = {
+#    Name = var.component_name
+#  }
 }
 
 resource "aws_route53_record" "dns_records" {
